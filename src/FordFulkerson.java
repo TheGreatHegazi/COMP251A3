80ac87dcd12209e8f900f34d8185e85493d7c8a9
Ahmed <hegaziahmed@outlook.com>
2018-11-05 21:46:10 -0500

    preliminary functions for FFA

--- src/FordFulkerson.java
+++ src/FordFulkerson.java
@@ -6,47 +6,51 @@ public class FordFulkerson {
 	public static ArrayList<Integer> pathDFS(Integer source, Integer destination, WGraph graph) {
 		ArrayList<Integer> Stack = new ArrayList<Integer>();
 		ArrayList<Integer> nodes = new ArrayList<Integer>();
-		
+
 		nodes = getNodes(graph);
 		int[] visited = new int[nodes.size()];
-		
-		 DFS(source, destination , graph , visited, Stack);
-		
-		return Stack;
+
+		DFS(source, destination, graph, visited, Stack);
+
+		if (Stack.get(Stack.size() - 1) == destination) {
+			return Stack;
+		} else {
+			Stack.removeAll(nodes);
+			return Stack;
+		}
 	}
-	public static void DFS (Integer source, Integer destination, WGraph graph, int[] visited, ArrayList<Integer> stack){
+
+	public static void DFS(Integer source, Integer destination, WGraph graph, int[] visited, ArrayList<Integer> stack) {
 		ArrayList<Integer> adj = new ArrayList<Integer>();
 		ArrayList<Integer> nodes = new ArrayList<Integer>();
-		
+
 		nodes = getNodes(graph);
 		adj = getAdj(nodes.get(source), graph);
 		visited[source]++;
 		stack.add(nodes.get(source));
-		for (Integer node : adj){
-			if (nodes.get(node) != null && visited[node] ==0){
-			DFS(node, destination, graph, visited, stack);
+		for (Integer node : adj) {
+			if (nodes.get(node) != null && visited[node] == 0) {
+				DFS(node, destination, graph, visited, stack);
 
 			}
-		
 
 		}
 	}
 
-
-	public static ArrayList<Integer> getAdj (int node, WGraph g){
-		ArrayList<Integer> adj= new ArrayList<Integer>();
-		for (Edge e: g.getEdges()){
+	public static ArrayList<Integer> getAdj(int node, WGraph g) {
+		ArrayList<Integer> adj = new ArrayList<Integer>();
+		for (Edge e : g.getEdges()) {
 			for (int n : e.nodes)
-			if (n == node)
-				if (e.nodes[0]==node){
-					adj.add(e.nodes[1]);
-				}
-				else{
-				adj.add(e.nodes[0]);
-				}
+				if (n == node)
+					if (e.nodes[0] == node) {
+						adj.add(e.nodes[1]);
+					} else {
+						adj.add(e.nodes[0]);
+					}
 		}
 		return adj;
 	}
+
 	public static ArrayList<Integer> getNodes(WGraph g) {
 		ArrayList<Integer> nodes = new ArrayList<Integer>();
 		for (Edge e : g.getEdges()) {
@@ -62,19 +66,44 @@ public class FordFulkerson {
 	}
 
 	public static void fordfulkerson(Integer source, Integer destination, WGraph graph, String filePath) {
+	
 		String answer = "";
 		String myMcGillID = "260725697"; // Please initialize this variable with
 											// your McGill ID
 		int maxFlow = 0;
+		int[][] forwardEdge = new int[(graph.getNbNodes())][(graph.getNbNodes())];
+		
+		
+		
+		WGraph graphF = new WGraph();
+		ArrayList<Edge> edgesF = graphF.getEdges();
+		for (Edge e : edgesF) {
+			e.weight = 0;
+		}
 
-		pathDFS(source, destination, graph);
-		/*
-		 * YOUR CODE GOES HERE // // // // // // //
-		 */
+		
+		ArrayList<Integer> path = pathDFS(source, destination, graph);
+		
+		if( path .isEmpty()){
+			answer += maxFlow + "\n" + graph.toString();
+			writeAnswer(filePath + myMcGillID + ".txt", answer);
+			System.out.println(answer);
+		}
+		
+		
+		int throttle = 55555555;
+		ArrayList<Edge> edgecapacity = graph.getEdges();
+		for (Edge e : edgecapacity){
+			throttle = Math.min(throttle, e.weight);
+		}
+		
+		for (int i = 0; i < path.size()-1; i++){
+			Edge e = graphF.getEdge(path.get(i), path.get(i+1));
+			e.weight = throttle;
+		}
+		
+		
 
-		answer += maxFlow + "\n" + graph.toString();
-		writeAnswer(filePath + myMcGillID + ".txt", answer);
-		System.out.println(answer);
 	}
 
 	public static void writeAnswer(String path, String line) {
